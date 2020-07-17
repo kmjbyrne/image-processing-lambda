4e15d11410cd1ae9c3af6def7fc876d3023d1b7e
Keith Byrne <keithmbyrne@gmail.com>
2020-07-17 22:51:12 +0100

    commit changes

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from src.handler import main
 
 data = """
+https://s3-eu-west-1.amazonaws.com/bns.assets/public/images/IMG_20200517_194237.jpg
+https://s3-eu-west-1.amazonaws.com/bns.assets/public/images/2020/april/IMG_20200517_194237.jpg
 """
 
 for item in data.split('\n'):
@@ -8,6 +10,8 @@ for item in data.split('\n'):
         item = item.replace('\'', '')
         item.replace("'", '')
         item = item.replace('https://s3-eu-west-1.amazonaws.com/bns.assets/', '')
+        # event = {'eventVersion': '2.1', 'eventSource': 'aws:s3', 'awsRegion': 'eu-west-1', 'eventTime': '2020-07-09T22:15:33.472Z', 'eventName': 'ObjectCreated:Put', 'userIdentity': {'principalId': 'AHSNSMR94E2B8'}, 'requestParameters': {'sourceIPAddress': '84.247.48.74'}, 'responseElements': {'x-amz-request-id': 'ENAXAS4K4Y0HDN9J', 'x-amz-id-2': '6Xt41dKGjU6nFz3wR5DjJQ/eCvmuERqC1jjVtgzSGj7fP09wRHJbfKylp10Sxo1s35bF7b0S8YNaCZMkPmV5Las7Dgu1GpDH'}, 's3': {'s3SchemaVersion': '1.0', 'configurationId': 'ImageProcessingService-10697522e2feeb9a024a57b384e290aa', 'bucket': {'name': 'bns.assets', 'ownerIdentity': {'principalId': 'AHSNSMR94E2B8'}, 'arn': 'arn:aws:s3:::bns.assets'}, 'object': {'key': 'public/images/IMG_20200517_194237.jpg', 'size': 2746948, 'eTag': 'b7a6d22c86198f246b5b280de7fabb4c', 'sequencer': '005F0797069E56726A'}}}
+        # main(event)
         main({'bucket': 'bns.assets', 'key': item, 'acl': 'public-read', 'thumbnail': True}, '')
         print(item)
 
--- src/handler.py
+++ src/handler.py
@@ -25,7 +25,8 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 root.addHandler(handler)
 
-
+BUCKET_PATH = 'public/images'
+THUMBNAIL_PATH = 'public/thumbnails'
 PORTRAIT_MAX = dict(x=600, y=1200)
 LANDSCAPE_MAX = dict(x=1200, y=600)
 
@@ -34,7 +35,7 @@ def extract_extension(key):
     return key.split('.').pop()
 
 
-def s3upload(file, bucket, key, acl=None):
+def s3upload(file, bucket, key, acl='public-read'):
     if not acl:
         acl = 'private'
     return s3client.put_object(Body=file, Bucket=bucket, Key=key, ACL=acl)
@@ -65,14 +66,7 @@ def resize_file(name, localfile, x, y):
     image.save(f'{localfile}')
 
 
-def main(event, context):
-    key = event.get('key', None)
-    bucket = event.get('bucket', None)
-    acl = event.get('acl', None)
-
-    if not key or not bucket:
-        raise ValueError('Function requires an S3 Bucket name & Object key to operate')
-
+def process(bucket, key, thumbnail=True, acl=None):
     try:
         resp = s3client.get_object(Bucket=bucket, Key=key)
     except errorfactory.ClientError as error:
@@ -94,18 +88,37 @@ def main(event, context):
         filestream.close()
 
     shutil.copyfile(localfile, copy)
-
     resize_file(name, localfile, 1200, 625)
     s3upload(open(localfile, 'rb'), bucket, key, acl=acl)
 
-    if event.get('thumbnail', None):
+    if thumbnail:
         resize_file(name, copy, 600, 300)
-        keypath = keys = key.split('/')
-        keypath.insert(len(keys) - 1, 'thumbnail')
-        key = '/'.join(keys)
-        s3upload(open(copy, 'rb'), bucket, key, acl=acl)
+        # keypath = keys = key.split('/')
+        # keypath.insert(len(keys) - 1, 'thumbnail')
+        # key = '/'.join(keys)
+        thumbnail_key = key.replace(BUCKET_PATH, THUMBNAIL_PATH)
+        s3upload(open(copy, 'rb'), bucket, thumbnail_key, acl=acl)
 
     os.remove(localfile)
+    return True
+
+
+def main(event, context):
+    if event.get('Records', None):
+        for record in event['Records']:
+            bucket = record['s3']['bucket']['name']
+            key = record['s3']['object']['key']
+
+            if not key or not bucket:
+                raise ValueError('Function requires an S3 Bucket name & Object key to operate')
+            process(bucket, key, True)
+    else:
+        key = event.get('key', None)
+        bucket = event.get('bucket', None)
+        acl = event.get('acl', None)
+        thumbnail = event.get('thumbnail', True)
+        process(bucket, key, thumbnail, acl)
+
     return dict(
         status=200,
         message='Image resize successful'
